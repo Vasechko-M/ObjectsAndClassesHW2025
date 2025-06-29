2ee02725981999bc2b5c1f27f021539b957f4f2f
MaryPV <fornewstudies@gmail.com>
2025-06-29 14:23:05 +0500

    Finish HW2

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private  String firstName;
     private  String middleName;
@@ -25,5 +27,18 @@ public class Author {
     @Override
     public String toString() {
         return "Автор{" + firstName + " " + middleName + " " + lastName + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(firstName, author.firstName) && Objects.equals(middleName, author.middleName) && Objects.equals(lastName, author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, middleName, lastName);
     }
 }
